bba97a99cce0bf920d66469f553e24dcae381a96
arnald18 <acenaldy04@gmail.com>
2023-08-02 16:20:15 -0400

    add route to notes page

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const html = require("./routes/html");
 
 const PORT = 3001;
 
@@ -9,6 +10,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
+
+app.use("/", html);
 
 //  START THE SERVER
 app.listen(PORT, () =>
