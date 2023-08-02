11f2b206c3eeb51ea5dcfcdf64d0018f6e931814
arnald18 <acenaldy04@gmail.com>
2023-08-02 19:04:33 -0400

    add POST /notes

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 
 const { join } = require("path");
 const { readFile, writeFile } = require("fs/promises");
+const { v4: uuidv4 } = require("uuid");
 
 // GET THE PATH OF THE DATABASE
 const databasePath = join(__dirname, "..", "db/db.json");
@@ -11,19 +12,20 @@ async function readDatabase() {
     return JSON.parse(data);
   });
 }
-
+// Save a note to the database
 async function addToDatabase(newNote) {
   return readDatabase().then(async (notes) => {
     let newNotes = notes;
-    newNotes.push(newNotes);
+    newNotes.push(newNote);
     return writeFile(databasePath, JSON.stringify(newNotes)).then(() => {
       console.log("Add to File");
     });
   });
 }
-
+// Get all the notes
 router.get("/notes", (req, res) => {
   console.info("Get request received");
+
   readDatabase()
     .then((notes) => {
       console.info("Success, sending notes");
@@ -33,6 +35,34 @@ router.get("/notes", (req, res) => {
       console.info("Failure, sending error");
       return res.status(500).json(err);
     });
+});
+
+router.post("/notes", (req, res) => {
+  const { title, text } = req.body;
+  if (!(title && text)) {
+    console.info("Failure, title and text requirements");
+    return res.status(400);
+  }
+
+  const newNote = {
+    title,
+    text,
+    id: uuidv4(),
+  };
+
+  addToDatabase(newNote)
+    .then(() => {
+      console.info("Success, added note to database");
+      return res.status(201);
+    })
+    .catch((err) => {
+      console.info("Failure, could not save new note");
+      return res.status(500).json(err);
+    });
+});
+
+router.post("/notes/:id", (req, res) => {
+  //
 });
 
 module.exports = router;
